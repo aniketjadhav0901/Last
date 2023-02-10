05a9ec27089b70aba9db82991e9f1d244d18cfea
aniketjadhav0901 <aniketrjadhav1996@gmail.com>
2023-02-10 17:41:53 +0530

    updated sub example

--- Latest/src/com/vella/Sub.java
+++ Latest/src/com/vella/Sub.java
@@ -1,13 +1,18 @@
 package com.vella;
 
 public class Sub {
+	public static void subtraction(int num1,int num2) {
+		int result;
+		result = num1 - num2;
+		System.out.println("subtraction is "+result);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a = 10;
 		int b = 5;
-		int c = a-b;
-		System.out.println("substraction is "+c);
+		
+		Sub.subtraction(a,b);
 	}
 
 }
