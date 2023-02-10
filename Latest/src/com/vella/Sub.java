e7a43a0100cd1bb46161ebd999cf1c4ed616a77e
aniketjadhav0901 <aniketrjadhav1996@gmail.com>
2023-02-10 13:56:45 +0530

    subtraction of two number

---
+++ Latest/src/com/vella/Sub.java
@@ -0,0 +1,13 @@
+package com.vella;
+
+public class Sub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 5;
+		int c = a-b;
+		System.out.println("substraction is "+c);
+	}
+
+}
