c5a2012b6e5fc471b49ad5642ad13f3c148b4bc3
aniketjadhav0901 <aniketrjadhav1996@gmail.com>
2023-02-14 11:21:21 +0530

    Right Trigle pattern interview program

---
+++ Latest/src/com/vella/InterView1.java
@@ -0,0 +1,17 @@
+package com.vella;
+
+public class InterView1 {
+
+	public static void main(String args[])   
+	{   
+	int row = 6;
+	for(int i=1;i<row;i++) {
+		for(int j=1;j<=i;j++) {
+			System.out.print("* ");
+		}
+		System.out.println();
+	}
+
+	}
+
+}
